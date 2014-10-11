a054d89e5fe2ccf1e55315dd671807407c3f35b2
Juan Ángel Moreno Fernández <jamf.dev@gmail.com>
2014-10-11 10:17:42 +0200

    New Texture Manager class
    The TextureManager will be the responsible of textures management.

---
+++ src/core/bmke/texture_manager.cpp
@@ -0,0 +1,144 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * bmonkey
+ * Copyright (C) 2014 Juan Ángel Moreno Fernández
+ *
+ * bmonkey is free software.
+ *
+ * You can redistribute it and/or modify it under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * bmonkey is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bmonkey.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "texture_manager.hpp"
+#include <cassert>
+
+namespace bmonkey{
+
+
+TextureManager::TextureManager(void):
+	m_smooth(false)
+{
+}
+
+TextureManager::~TextureManager(void)
+{
+	clear();
+}
+
+sf::Texture* TextureManager::loadTexture(const Glib::ustring& file, const bool repeated)
+{
+	std::unordered_map<std::string, Resource >::iterator iter;
+	Resource resource;
+
+	if (file.empty())
+	{
+		return nullptr;
+	}
+
+	// Comprobamos si ya tenemos la textura cargada
+	iter = m_textures.find(file);
+	if (iter != m_textures.end() )
+	{
+		// Incrementamos contador de referencias
+		++iter->second.count;
+		iter->second.texture->setRepeated(repeated);
+		return iter->second.texture;
+	}
+	else
+	{
+		resource.texture = new sf::Texture();
+		// Cargamos la nueva textura y la almacenamos
+		if (!resource.texture->loadFromFile(file))
+		{
+			delete resource.texture;
+			return nullptr;
+		}
+		else
+		{
+			resource.count = 1;
+			resource.texture->setRepeated(repeated);
+			resource.texture->setSmooth(m_smooth);
+			m_textures[file] = resource;
+		}
+		return resource.texture;
+	}
+}
+
+sf::Texture* TextureManager::getTexture(const Glib::ustring& file)
+{
+	std::unordered_map<std::string, Resource >::iterator iter;
+
+	if (file.empty())
+	{
+		return nullptr;
+	}
+
+	// Buscamos la textura
+	iter = m_textures.find(file);
+	if (iter != m_textures.end() )
+	{
+		return iter->second.texture;
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+void TextureManager::deleteTexture(const Glib::ustring& file)
+{
+	std::unordered_map<std::string, Resource >::iterator iter;
+
+	if (file.empty())
+	{
+		return;
+	}
+
+	// Buscamos la textura
+	iter = m_textures.find(file);
+	if (iter != m_textures.end() )
+	{
+		// Decrementamos el contador de referencias
+		--iter->second.count;
+		// Si no hay más referencias descargamos la textura
+		if (iter->second.count == 0)
+		{
+			delete iter->second.texture;
+			m_textures.erase(iter);
+		}
+	}
+}
+
+void TextureManager::setSmooth(const bool smooth)
+{
+	std::unordered_map<std::string, Resource >::iterator iter;
+
+	for (iter = m_textures.begin(); iter != m_textures.end(); ++iter)
+	{
+		iter->second.texture->setSmooth(smooth);
+	}
+	m_smooth = smooth;
+}
+
+void TextureManager::clear(void)
+{
+	std::unordered_map<std::string, Resource >::iterator iter;
+
+	for (iter = m_textures.begin(); iter != m_textures.end(); ++iter)
+	{
+		delete iter->second.texture;
+	}
+	m_textures.clear();
+}
+
+} // namespace bmonkey
---
+++ src/core/bmke/texture_manager.hpp
@@ -0,0 +1,104 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * bmonkey
+ * Copyright (C) 2014 Juan Ángel Moreno Fernández
+ *
+ * bmonkey is free software.
+ *
+ * You can redistribute it and/or modify it under the terms of the
+ * GNU General Public License, as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * bmonkey is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bmonkey.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef _TEXTURE_MANAGER_HPP_
+#define _TEXTURE_MANAGER_HPP_
+
+#include <SFML/Graphics.hpp>
+#include <glibmm/ustring.h>
+#include <unordered_map>
+#include "../../defines.hpp"
+
+namespace bmonkey{
+
+/**
+ * Gestiona las texturas del sistema
+ *
+ * Se encarga de mantener y gestionar las texturas que serán usadas por el
+ * frontend.
+ * Actua como una especie de almacén para las texturas que se van cargando desde
+ * fichero, proporcionando métodos para su carga y descarga.
+ */
+class TextureManager
+{
+public:
+
+	/**
+	 * Constructor de la clase
+	 */
+	TextureManager(void);
+
+	/**
+	 * Destructor de la clase
+	 */
+	virtual ~TextureManager(void);
+
+	/**
+	 * Carga una textura en el manager y la devuelve
+	 * @param file Path del fichero de la textura
+	 * @return Textura cargada o null si no se pudo cargar
+	 * @note Si la textura ya existe en el manager, no volverá a cargarla
+	 * @note La lista de formatos de imagenes soportados es la siguiente: bmp,
+	 * png, tga, jpg, gif, psd, hdr y pic. Algunas opciones de determinados
+	 * formatos no son soportadas, como el jpeg progresivo
+	 */
+	sf::Texture* loadTexture(const Glib::ustring& file, const bool repeated = false);
+
+	/**
+	 * Devuelve una textura indexada por su fichero
+	 * @param file Path del fichero de la textura
+	 * @return Textura cargada o null si no se encuentra
+	 */
+	sf::Texture* getTexture(const Glib::ustring& file);
+
+	/**
+	 * Elimina una textura del almacen interno del manager
+	 * @param file Path del fichero de la textura
+	 */
+	void deleteTexture(const Glib::ustring& file);
+
+	/**
+	 * Activa o desactiva el filtro de suavizado en las texturas
+	 * @param smooth Indica si se debe activar o no el filtro
+	 */
+	void setSmooth(const bool smooth);
+
+	/**
+	 * Limpia TextureManager liberando todas las texturas cargadas
+	 */
+	void clear(void);
+
+private:
+
+	// Estructura para poder llevar el recuento de referencias de la textura
+	struct Resource
+	{
+		unsigned int count;
+		sf::Texture* texture;
+	};
+
+	std::unordered_map<std::string, Resource > m_textures; /**< Almacen de texturas */
+	bool m_smooth;
+};
+
+} // namespace bmonkey
+
+#endif // _TEXTURE_MANAGER_HPP_
